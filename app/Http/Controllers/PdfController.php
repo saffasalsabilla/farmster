360a0eee0bcfc127e2df1dc12c069950d244bc1b
Saffa Salsabilla <55771189+saffasalsabilla@users.noreply.github.com>
2021-03-25 00:20:55 +0700

    Update PdfController.php

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use \PDF;
+use Barryvdh\DomPDF\PDF;
 
 class PdfController extends Controller
 {
-    // public function print()
-    // {
-    //     $pdf = PDF::loadview('post.index')->setPaper('A4', 'potrait');
-    //     return $pdf->download('laporan-post.pdf');
-    // }
+    public function print()
+    {
+        $pdf = \PDF::loadview('post.index')->setPaper('A4', 'potrait');
+        return $pdf->download('laporan-post.pdf');
+    }
 }
